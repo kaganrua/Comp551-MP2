5d1930d8ece85b668f18bb9f5c4197d8d654b1cb
kaganrua <45906255+kaganrua@users.noreply.github.com>
2021-02-18 16:01:00 -0500

    Add files via upload

--- src/LogisticRegression.py
+++ src/LogisticRegression.py
@@ -20,18 +20,30 @@ def main():
     print('main')
     df_train = pd.read_csv(osp.join('..' , 'data' , '20_News' , '20_News_Train_Preprocessed.csv') , index_col=False)
     df_test = pd.read_csv(osp.join('..', 'data', '20_News', '20_News_Test_Preprocessed.csv'), index_col=False)
+    df_train = df_train.drop(df_train.columns[[0]] , axis=1)
+    print(df_train)
+
+
+    tv = TfidfVectorizer(max_df=1., min_df=3, max_features=20000)
 
 
 
-    cv = CountVectorizer(max_df=0.5, min_df=2, max_features=300)
-
-    print(df_train['Text'])
-
-    X_train = cv.fit_transform(df_train['Text']).toarray()
+    X_train = tv.fit_transform(df_train['Text']).toarray()
     Y_train = df_train['Class']
-    #X_train = SelectKBest(chi2, k=5000).fit_transform(X_train, Y_train)
-    print(cv.get_feature_names())
-    print(X_train[1][:-1])
+    #X_train = SelectKBest(chi2, k=3000).fit_transform(X_train, Y_train)
+    #print(tv.get_feature_names())
+    X_test = df_test['Text']
+    X_test = tv.transform(X_test).toarray()
+    Y_test = df_test['Class']
+    print(X_train)
+    clf = LogisticRegression(max_iter=3000)
+
+    clf.fit(X_train, Y_train)
+
+    y_pred = clf.predict(X_test)
+
+    acc = accuracy_score(Y_test , y_pred)
+    print(acc)
 
 
 
